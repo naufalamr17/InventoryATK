85df40b0edd8585af1aec5e1a471f69f4cb7dbbb
naufalamr17 <naufal.amr17@outlook.co.id>
2024-07-22 08:18:24 +0700

    Change minor update

--- resources/views/pages/asset/inputasset.blade.php
+++ resources/views/pages/asset/inputasset.blade.php
@@ -55,7 +55,11 @@
 
                                         <div class="form-group">
                                             <label for="pic">PIC</label>
-                                            <input id="pic" class="form-control border p-2" type="text" name="pic" value="{{ old('pic') }}" required>
+                                            <input id="pic" class="form-control border p-2" type="text" name="pic" list="picList" required>
+                                            <datalist id="picList">
+                                                <option value="Reggie">
+                                                <option value="Baya">
+                                            </datalist>
                                             @if ($errors->has('pic'))
                                             <div class="text-danger mt-2">{{ $errors->first('pic') }}</div>
                                             @endif
